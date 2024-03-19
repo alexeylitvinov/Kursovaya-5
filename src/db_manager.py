974be123a45837a0a522e0c76031a4bd8a7cf2e2
alexeylitvinov <alextwin1976@gmail.com>
2024-03-19 19:17:39 +0100

    create all func for class

--- src/db_manager.py
+++ src/db_manager.py
@@ -11,14 +11,16 @@ class DBManager:
     def create_database(self, db_name):
         try:
             self.conn.autocommit = True
+            self.cur = self.conn.cursor()
+            self.cur.execute(f'DROP DATABASE IF EXISTS {db_name};')
             self.cur.execute(f'CREATE DATABASE {db_name};')
+            print('DB create')
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
             self.cur.close()
             self.conn.close()
-            self.param_connect['dbname'] = db_name
-            self.conn = psycopg2.connect(**self.param_connect)
+            self.conn = psycopg2.connect(dbname=db_name, **self.param_connect)
             self.cur = self.conn.cursor()
 
     def create_table_employers(self):
@@ -28,6 +30,7 @@ class DBManager:
                                 employer_id int,
                                 employer_name varchar,
                                 CONSTRAINT pk_employers_vacancy_id PRIMARY KEY (vacancy_id));''')
+            print('Table employers create')
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
@@ -47,6 +50,7 @@ class DBManager:
                                 CONSTRAINT pk_vacancies_vacancy_id PRIMARY KEY (vacancy_id));
                                 ALTER TABLE vacancies ADD CONSTRAINT fk_vacancies_vacancy_id FOREIGN KEY (vacancy_id)
                                 REFERENCES employers(vacancy_id);''')
+            print('Table vacancies create')
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
@@ -81,11 +85,33 @@ class DBManager:
             print(error)
         get_table_rows(self.cur)
 
-    def get_avg_salary(self):
-        pass
-
     def get_vacancies_with_higher_salary(self):
-        pass
+        try:
+            self.cur.execute('''SELECT vacancy_id, vacancy_name, roles_name, salary_from, salary_to 
+                                FROM vacancies
+                                WHERE salary_from > (SELECT AVG (salary_from) FROM vacancies)
+                                ORDER BY salary_from;''')
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        get_table_rows(self.cur)
 
-    def get_vacancies_with_keyword(self):
-        pass
+    def get_avg_salary(self):
+        try:
+            self.cur.execute('SELECT AVG (salary_from) FROM vacancies')
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        get_table_rows(self.cur)
+
+    def get_vacancies_with_keyword(self, key_world):
+        try:
+            self.cur.execute(f'''SELECT vacancy_id, vacancy_name, roles_name, url
+                                FROM vacancies 
+                                WHERE vacancy_name LIKE '%{key_world.lower()}%' 
+                                OR vacancy_name LIKE '{key_world.title()}%'
+                                OR vacancy_name LIKE '%{key_world.lower}'
+                                OR roles_name LIKE '%{key_world.lower}%'
+                                OR roles_name LIKE '{key_world.title}%'
+                                OR roles_name LIKE '%{key_world.lower}';''')
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        get_table_rows(self.cur)
--- src/main.py
+++ src/main.py
@@ -7,38 +7,24 @@ CONFIG_FILE = '../conf/config.ini'
 response_keys = get_config(CONFIG_FILE, 'hh_ru')
 employees_id = get_config(CONFIG_FILE, 'employees_id')
 url = get_config(CONFIG_FILE, 'url')['url']
+
 db = DBManager(CONFIG_FILE)
-# db.create_database('db_hhru')
-# print(db.param_connect)
-# db.create_table_employers()
-# db.create_table_vacancies()
+db.create_database('db_hhru')
+db.create_table_employers()
+db.create_table_vacancies()
 
-# print(db.param_connect)
-# for i in employees_id:
-#     response_keys['employer_id'] = i
-#     response = requests.get(url, response_keys)
-#     data = response.json()
-#     for j in data['items']:
-        # in_table = get_data_for_employers(j)
-        # print(get_data_for_employers(j))
-        # print(get_data_for_vacancies(j))
-        # db.into_table(' employers', 3, get_data_for_employers(j))
-        # db.into_table('vacancies', 10, get_data_for_vacancies(j))
+print('Fill the table')
+for i in employees_id:
+    response_keys['employer_id'] = i
+    response = requests.get(url, response_keys)
+    data = response.json()
+    for j in data['items']:
+        db.into_table(' employers', 3, get_data_for_employers(j))
+        db.into_table('vacancies', 10, get_data_for_vacancies(j))
 
-db.get_companies_and_vacancies_count()
-db.get_all_vacancies()
+# db.get_companies_and_vacancies_count()
+# db.get_all_vacancies()
+# db.get_avg_salary()
+# db.get_vacancies_with_keyword('менеджер')
+# db.get_vacancies_with_higher_salary()
 db.close_connection()
-#         print(j['area']['id'])
-#         print(j['area']['name'])
-#         print(j['id'])
-#         print(j['name'])
-#         print(j['employer']['id'])
-#         print(j['employer']['name'])
-#         print(j['professional_roles'][0]['name'])
-#         print(j['salary']['from'])
-#         print(j['salary']['to'])
-#         print(j['salary']['currency'])
-#         print(j['alternate_url'])
-#         print(j['snippet']['requirement'])
-#         print(j['snippet']['responsibility'])
-#         print('=======================================================================================================')
